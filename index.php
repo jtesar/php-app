416bb32fd821e7ba3050a7431cc81c51401d2ced
jtesar <67164623+jtesar@users.noreply.github.com>
2022-06-06 11:53:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,4 @@
 <?php
- $servername = "mysql.jtesar-multi.svc.cluster.local";
- $serviceport = "3306";
- $username = "jtesar";
- $password = "redhat";
- $dbname = "db";
-
-  $conn = new mysqli($servername, $username, $password, $dbname);
-   if ($conn->connect_error) {
-                   die("Connection failed: " . $conn->connect_error);
-                    }
-
-   $sql = "select name from names";
-   $result = $conn->query($sql);
-
-   if ($result->num_rows > 0) {
-          while($row = $result->fetch_assoc()) {
-                                          echo "Name: " . $row["name"]."\n<br/>";
-                                                    }
-                    }
-   else {
-            echo "0 results";
-                      }
-   $conn->close();
+   $lines = system('curl www.root.cz');
+   echo $lines;
 ?>
