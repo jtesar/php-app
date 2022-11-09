46233aa17143d009a7960ed1fdbe10f9cdeb8749
Student User <student@workstation.lab.example.com>
2022-11-09 05:51:57 -0500

    dd

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <?php
-  echo "New version<br>";
-  echo "New version 2<br>";
-  echo "New version 3<br>";
-  echo "New version 4<br>";
-  phpInfo();
+ $servername = "mysql.jtesar-s2i.svc.cluster.local";
+ $serviceport = "3306";
+ $username = "jtesar";
+ $password = "redhat";
+ $dbname = "db";
 
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ if ($conn->connect_error) {
+	   die("Connection failed: " . $conn->connect_error);
+ }
+	  
+  $sql = "select name from names";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+	  while($row = $result->fetch_assoc()) {
+		  echo "Name: " . $row["name"]."\n";
+          }
+  } 
+  else {
+	    echo "0 results";
+  }
+  $conn->close();
 ?>
